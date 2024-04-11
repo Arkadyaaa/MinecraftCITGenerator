f86b4c432172806ccb757b1cb459530c5902d6be
Arkadyaaa <arkadyarkady100@gmail.com>
2024-04-11 09:55:48 +0800

    first version - items tab

---
+++ README.md
@@ -0,0 +1,10 @@
+# UNFINISHED
+This does not function at the moment. I am currently working on this. Currently only front-end proof of concept as of now.
+
+# MinecraftCITGenerator
+Generates CIT .properties files in batch
+
+Input every value you need in the tool and it will generate .properties for all the textures/models you provided.
+Note: You only need to input the fields you need. You can leave others blank and they will not be included in the file.
+
+- Optifine CIT Documentation: https://optifine.readthedocs.io/cit.html#properties 
---
+++ script.js
@@ -0,0 +1,27 @@
+// Function to open a tab tab
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Trigger a click on the first tab button when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the element with id "defaultOpen" (first tab button) and click it
+    document.getElementById("default-open").click();
+});
